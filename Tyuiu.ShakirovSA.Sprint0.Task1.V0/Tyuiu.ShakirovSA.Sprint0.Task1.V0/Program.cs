aa1c91dab34723544a50272548902abb77e9c687
Сергей Шакиров <sergej.shakirov.04@mail.ru>
2025-09-09 23:20:37 +0500

    + Task1

---
+++ Tyuiu.ShakirovSA.Sprint0.Task1.V0/Tyuiu.ShakirovSA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string name = "Сергей";
+int age = 17;
+Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
