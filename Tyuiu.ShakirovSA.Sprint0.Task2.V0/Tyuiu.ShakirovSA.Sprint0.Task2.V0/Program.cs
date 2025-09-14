5ff44d99c4a21b125615afc25461bc7adbf16935
Сергей Шакиров <sergej.shakirov.04@mail.ru>
2025-09-15 02:42:48 +0500

    + Task2

---
+++ Tyuiu.ShakirovSA.Sprint0.Task2.V0/Tyuiu.ShakirovSA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,9 @@
+﻿using Tyuiu.ShakirovSA.Sprint0.Task2.V0.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(DataService.GetMessage("Сергей"));
+        Console.ReadKey();
+    }
+}
